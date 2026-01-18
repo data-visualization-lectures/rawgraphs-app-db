faeea71d086850593231ca5dfa6f73b595703816
Yuichi Yazaki <visualizingjp@gmail.com>
2026-01-18 11:48:13 +0900

    ツールタイトルをロゴ画像に差し替え

--- src/components/FixedHeader/FixedHeader.js
+++ src/components/FixedHeader/FixedHeader.js
@@ -4,7 +4,13 @@ import styles from './FixedHeader.module.scss'
 export default function FixedHeader() {
     return (
         <div className={styles.header}>
-            <span className={styles.title}>RawGraphs</span>
+            <span className={styles.title}>
+                <img
+                    className={styles.logo}
+                    src="/logo_rawgraphs.png"
+                    alt="RawGraphs"
+                />
+            </span>
         </div>
     )
 }
