afe76d3f0eaba0b80fdb3fa1eb538c50fb10309a
Yuichi Yazaki <visualizingjp@gmail.com>
2026-02-08 16:33:28 +0900

    チュートリアルリンクの削除

--- src/custom_charts/histogram/metadata.js
+++ src/custom_charts/histogram/metadata.js
@@ -10,5 +10,5 @@ export const metadata = {
   description:
     'A histogram is an approximate representation of the distribution of numerical data. It was first introduced by Karl Pearson.',
   code: 'https://github.com/rawgraphs/rawgraphs-charts/tree/master/src/histogram',
-  tutorial: 'https://rawgraphs.io/learning/',
+
 }
